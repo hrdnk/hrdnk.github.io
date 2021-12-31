d64f2b76e0f3be451401a5e9508f3a92080ad14a
hrdnk <h.hrdnk@gmail.com>
2021-12-31 23:06:20 +0200

    no message

--- fubbaucubbacubba/js/checkout-page.js
+++ fubbaucubbacubba/js/checkout-page.js
@@ -1,10 +1,10 @@
 // nice select for select
 $(document).ready(function() {
     $('.card-info').niceSelect();
-    $('.checkout-data-helper__button').on('mouseenter mouseleave touchstart', function(event){
+    $('.checkout-data-helper__button').on('mouseenter mouseleave', function(event){
         // detectTap = true;
         $('.checkout-data-helper__tooltip').toggleClass('show');
-        if (event.type == "touchend") detectTap = true; // Detects click events
+        if (event.type == "touchstart") detectTap = false; // Detects click events
         if (detectTap){
            // Here you can write the function or codes you want to execute on tap
            $('.checkout-data-helper__tooltip').removeClass('show');
