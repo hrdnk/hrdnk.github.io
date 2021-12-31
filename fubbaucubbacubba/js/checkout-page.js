384fe824495b7500246c85e2e8eb235ed110049a
hrdnk <h.hrdnk@gmail.com>
2021-12-31 22:49:09 +0200

    no message

--- fubbaucubbacubba/js/checkout-page.js
+++ fubbaucubbacubba/js/checkout-page.js
@@ -1,9 +1,14 @@
 // nice select for select
 $(document).ready(function() {
     $('.card-info').niceSelect();
-    $('.checkout-data-helper__button').on('mouseenter mouseleave touchstart touchend', function(){
-        detectTap = true;
+    $('.checkout-data-helper__button').on('mouseenter mouseleave touchstart', function(event){
+        // detectTap = true;
         $('.checkout-data-helper__tooltip').toggleClass('show');
+        if (event.type == "touchend") detectTap = true; // Detects click events
+        if (detectTap){
+           // Here you can write the function or codes you want to execute on tap
+           $('.checkout-data-helper__tooltip').removeClass('show');
+        }
     })
 });
 // end nice select for select
