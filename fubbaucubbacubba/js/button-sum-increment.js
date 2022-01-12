585a75d77060c8d6746d67ec9a8537d3934602a6
hrdnk <h.hrdnk@gmail.com>
2022-01-12 18:53:27 +0200

    no message

--- fubbaucubbacubba/js/button-sum-increment.js
+++
@@ -1,6 +0,0 @@
-$('.btn-sum__add').click(function () {
-    $(this).prev().text(+$(this).prev().text() + 1);
-});
-$('.btn-sum__remove').click(function () {
-    if ($(this).next().text() > 0) $(this).next().text(+$(this).next().text() - 1);
-});
