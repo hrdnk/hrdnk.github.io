703b81e5249eaa09fae15b8390d47a092c9bddbd
hrdnk <h.hrdnk@gmail.com>
2022-01-15 16:54:13 +0200

    add remake portfolio

---
+++ js/gifs.js
@@ -0,0 +1,14 @@
+
+// list projects gifs --first section
+// $(".works_list-item a").hover(function(){
+//     $(this).parent().find("img").addClass("is-hover");
+//     }, function() {
+//     $(this).parent().find("img").removeClass("is-hover");
+// });
+
+// "Michael" gif --last section
+$(".about-hover").hover(function(){
+    $(this).parent().find(".about-hover__img").addClass("is-hover");
+    }, function() {
+    $(this).parent().find(".about-hover__img").removeClass("is-hover");
+});
